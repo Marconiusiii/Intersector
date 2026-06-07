af5cbff7281acd9bee291cc1438f53cf262f5027
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-06-07 15:57:19 -0700

    Initial commit of Intersector

---
+++ Intersector/IntersectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntersectorApp.swift
+//  Intersector
+//
+//  Created by Marco Salsiccia on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct IntersectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntersectorTests/IntersectorTests.swift
@@ -0,0 +1,19 @@
+//
+//  IntersectorTests.swift
+//  IntersectorTests
+//
+//  Created by Marco Salsiccia on 6/7/26.
+//
+
+import Testing
+@testable import Intersector
+
+struct IntersectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
